408649046979d1f804ebc2f461df497df7426905
Alaric Cole <alariccole@users.noreply.github.com>
2024-03-16 14:39:38 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,11 @@
 document.querySelectorAll('.icon-container').forEach(icon => {
-  icon.addEventListener('touchstart', () => {
-    icon.classList.add('touched');
-  });
-  icon.addEventListener('touchend', () => {
-    icon.classList.remove('touched');
-  });
-});
+  const updateAnimation = () => {
+    const scaleValue2 = 1 + Math.random() * 0.1;
+    const scaleValue3 = 1 + Math.random() * 0.1;
+    icon.style.setProperty('--random-scale2', scaleValue2);
+    icon.style.setProperty('--random-scale3', scaleValue3);
+  };
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.icon-container').forEach((icon, index) => {
-    icon.addEventListener('touchstart', () => {
-      const scaleValue2 = 1 + Math.random() * 0.1; // Random scale between 1.0 and 1.1
-      const scaleValue3 = 1 + Math.random() * 0.1; // Random scale between 1.0 and 1.1
-      icon.style.setProperty('--random-scale2', scaleValue2);
-      icon.style.setProperty('--random-scale3', scaleValue3);
-    });
-  });
+  icon.addEventListener('touchstart', updateAnimation);
+  icon.addEventListener('mouseenter', updateAnimation); // For mouse hover on non-touch devices
 });
