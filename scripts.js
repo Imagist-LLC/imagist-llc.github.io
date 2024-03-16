957c411d2c7ce313ab2e06b14b068ddd6f7f19e2
Alaric Cole <alariccole@users.noreply.github.com>
2024-03-16 11:33:05 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-// scripts.js
+document.querySelectorAll('.parallax-icon').forEach(icon => {
+  icon.addEventListener('touchstart', () => {
+    icon.classList.add('touched');
+  });
+  icon.addEventListener('touchend', () => {
+    icon.classList.remove('touched');
+  });
+});
