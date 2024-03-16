6b05778d29087305251e667e1e0a08fe7c137486
Alaric Cole <alariccole@users.noreply.github.com>
2024-03-16 14:37:03 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,3 +6,15 @@ document.querySelectorAll('.icon-container').forEach(icon => {
     icon.classList.remove('touched');
   });
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.icon-container').forEach((icon, index) => {
+    icon.addEventListener('touchstart', () => {
+      const scaleValue2 = 1 + Math.random() * 0.1; // Random scale between 1.0 and 1.1
+      const scaleValue3 = 1 + Math.random() * 0.1; // Random scale between 1.0 and 1.1
+      icon.style.setProperty('--random-scale2', scaleValue2);
+      icon.style.setProperty('--random-scale3', scaleValue3);
+    });
+  });
+});
